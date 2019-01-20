49f65137ca7f93ecfececed5e33ba5583df84696
John Lovejoy <johnflovejoy@gmail.com>
2019-01-20 14:59:01 -0600

    This is me using the syntatic suger of short hand declaration with three types on one line.

--- variables/variableTut.go
+++ variables/variableTut.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Test")
+	//Using the short declaration syntax, I can assign 3 variables with different type in one line
+	a, b, c := 32, "James Bond", true
+	fmt.Println(a, b, c)
+
+	fmt.Printf("Hello, my name is %v and I am %v years old.\t This is all %v\n", b, a, c)
 }
